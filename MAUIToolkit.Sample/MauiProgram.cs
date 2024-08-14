4ca1ef9e807b201739408ec93826e5c59b80a6f3
Pham Tien <pham.tien@ucg.co.nz>
2024-08-14 13:42:47 +0700

    ned

--- MAUIToolkit.Sample/MauiProgram.cs
+++ MAUIToolkit.Sample/MauiProgram.cs
@@ -7,6 +7,7 @@ namespace MAUIToolkit.Sample
     {
         public static MauiApp CreateMauiApp()
         {
+            //mock comment
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
